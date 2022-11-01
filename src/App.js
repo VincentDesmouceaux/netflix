325889e51460237f02103850fc3c811f44eeab92
vincent desmouceaux <vincentdesmont@mbp-de-vincent.lan>
2022-11-01 15:52:30 +0100

    update4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import "./App.css";
-
+import data from "./users.json";
 import netflixLogo from "./img/netflix.png";
 function App() {
+  const movies = [{ category, images }];
   return (
-    <div className="App">
-      <img alt="netflixLogo" src={netflixLogo} />
-      <Container />
-    </div>
+    <ul>
+      {data.map((element, index) => {
+        return <> {element.movies}</>;
+      })}
+    </ul>
   );
 }
 
 export default App;
+
+// const App = () => {
+//   const users = ["Bastien", "Tom"];
+
+//   return (
+//     <ul>
+//       {users.map((name) => {
+//         return <p>{name}</p>;
+//       })}
+//     </ul>
+//   );
+// };
+
+// export default App;
